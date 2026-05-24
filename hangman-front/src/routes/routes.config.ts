d96f0a2db557d4a8a570ed5e484634fa2397b8c6
David Hormigo Ramírez <dhorram948@g.educaand.es>
2026-05-24 19:20:47 +0200

    Fix rama y test

--- hangman-front/src/routes/routes.config.ts
+++ hangman-front/src/routes/routes.config.ts
@@ -1,7 +1,6 @@
 import { Application } from 'express';
 import { topicsRoute } from './topics.route';
 
-// TODO CAMBIOS EN ROUTAS
 export const routesInitialization = (app: Application) => {
     app.use('/api/topics', topicsRoute());
 };
